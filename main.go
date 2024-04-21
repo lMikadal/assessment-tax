719f88bd7fbc8f3ce4b15eb71c355fc14274bd55
Mikada <panupong.mikada@gmail.com>
2024-04-21 20:43:54 +0700

    create graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/lMikadal/assessment-tax/postgres"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
@@ -11,7 +17,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	_ = db
-	fmt.Printf("Connected to postgres database\n")
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
+	})
+
+	// Start server
+	go func() {
+		post := ":" + os.Getenv("PORT")
+		if err := e.Start(post); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
