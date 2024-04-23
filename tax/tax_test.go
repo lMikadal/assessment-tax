8d4c3aa54b0d28305bd94312d7c641a7216edb1e
Mikada <panupong.mikada@gmail.com>
2024-04-23 21:12:47 +0700

    add test case wht less than 0

--- tax/tax_test.go
+++ tax/tax_test.go
@@ -117,4 +117,46 @@ func TestTaxHandler(t *testing.T) {
 			t.Errorf("got: %v, want: %v", got, want)
 		}
 	})
+
+	t.Run("Test Wht less than 0", func(t *testing.T) {
+		e := echo.New()
+		MockReq := ReqTax{
+			TotalIncome: 1.0,
+			Wht:         -1.0,
+			Allowances: []Allowance{
+				{
+					AllowanceType: "donation",
+					Amount:        0.0,
+				},
+			},
+		}
+		reqBody, _ := json.Marshal(MockReq)
+		req := httptest.NewRequest(http.MethodPost, "/tax/calculations", bytes.NewBuffer(reqBody))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		mock := MockTax{
+			db: []DB{},
+		}
+
+		handler := New(&mock)
+		handler.TaxHandler(c)
+
+		want := Err{Message: "Wht must be greater than 0"}
+		gotJson := rec.Body.Bytes()
+
+		var got Err
+		if err := json.Unmarshal(gotJson, &got); err != nil {
+			t.Errorf("failed to unmarshal json: %v", err)
+		}
+
+		if rec.Code != http.StatusBadRequest {
+			t.Errorf("got: %v, want: %v", rec.Code, http.StatusBadRequest)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
 }
