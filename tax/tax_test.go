e6ef555dda8a237b8fc8863822bdd8a8887b3c4d
Mikada <panupong.mikada@gmail.com>
2024-04-24 20:42:48 +0700

    add new test, test allowanceType k-receipt

--- tax/tax_test.go
+++ tax/tax_test.go
@@ -718,4 +718,65 @@ func TestTaxHandler(t *testing.T) {
 		}
 	})
 
+	t.Run("Test Income 50,000 and K-Receipt 100,000 output tax 24,000", func(t *testing.T) {
+		e := echo.New()
+		MockReq := ReqTax{
+			TotalIncome: 500000.0,
+			Wht:         0.0,
+			Allowances: []Allowance{
+				{
+					AllowanceType: "k-receipt",
+					Amount:        100000.0,
+				},
+			},
+		}
+		reqBody, _ := json.Marshal(MockReq)
+		req := httptest.NewRequest(http.MethodPost, "/tax/calculations", bytes.NewBuffer(reqBody))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		mock := MockTax{
+			db: []DB{
+				{Minimum_salary: 0, Maximum_salary: 150000, Rate: 0},
+				{Minimum_salary: 150001, Maximum_salary: 500000, Rate: 10},
+			},
+			dbDeduction: []DbDeduction{
+				{
+					ID:             1,
+					Type:           "Personal",
+					Minimum_amount: 10000,
+					Maximum_amount: 100000,
+					Amount:         60000,
+					Created_at:     "2021-09-01",
+					Updated_at:     "2021-09-01",
+				},
+				{
+					ID:             1,
+					Type:           "K-Receipt",
+					Minimum_amount: 0,
+					Maximum_amount: 100000,
+					Amount:         50000,
+					Created_at:     "2021-09-01",
+					Updated_at:     "2021-09-01",
+				},
+			},
+		}
+
+		handler := New(&mock)
+		handler.TaxHandler(c)
+
+		want := ResTax{Tax: 24000.0}
+		gotJson := rec.Body.Bytes()
+
+		var got ResTax
+		if err := json.Unmarshal(gotJson, &got); err != nil {
+			t.Errorf("failed to unmarshal json: %v", err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+
 }
