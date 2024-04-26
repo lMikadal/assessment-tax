5c05a0e55929f40d9ba4e8dbd7573dd083e6215b
Mikada <panupong.mikada@gmail.com>
2024-04-26 22:14:41 +0700

    add test same keyword first line

--- tax/tax_csv_err_test.go
+++ tax/tax_csv_err_test.go
@@ -60,4 +60,50 @@ func TestCsvErrorHander(t *testing.T) {
 			t.Errorf("got: %v, want: %v", got, want)
 		}
 	})
+
+	t.Run("Test first line same keyword", func(t *testing.T) {
+		e := echo.New()
+
+		body := new(bytes.Buffer)
+		writer := csv.NewWriter(body)
+		writer.Write([]string{"totalIncome", "totalIncome"})
+		writer.Flush()
+
+		req := httptest.NewRequest(http.MethodPost, "/tax/calculations/upload-csv", body)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		mock := MockTax{
+			dbDeduction: []DbDeduction{
+				{
+					Type:   "Personal",
+					Amount: 60000,
+				},
+				{
+					Type:   "Donation",
+					Amount: 100000,
+				},
+			},
+		}
+
+		handler := New(&mock)
+		handler.UploadCSVHandler(c)
+
+		want := Err{Message: "invalid csv"}
+		gotJson := rec.Body.Bytes()
+
+		var got Err
+		if err := json.Unmarshal(gotJson, &got); err != nil {
+			t.Errorf("failed to unmarshal json: %v", err)
+		}
+
+		if rec.Code != http.StatusBadRequest {
+			t.Errorf("got: %v, want: %v", rec.Code, http.StatusBadRequest)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
 }
